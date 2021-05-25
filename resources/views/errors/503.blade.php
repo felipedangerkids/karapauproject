29998092f6282af3ec1ce82a1de300297909c3da
felipe <felipephplow@gmail.com>
2021-05-25 11:47:25 -0300

    a

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -13,7 +13,7 @@
 <body>
       <div class="container mt-5 pt-5">
             <div class="alert alert-danger text-center">
-                  <h2 class="display-3">Manutenção</h2>
+                  <h2 class="display-3">503</h2>
                   <p class="display-5">Logo voltaremos, aguarde!</p>
             </div>
       </div>
