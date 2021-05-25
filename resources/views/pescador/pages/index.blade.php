6be8e520d2746b45a3184f2223a7e28293d0d61b
felipe <felipephplow@gmail.com>
2021-05-25 20:53:00 -0300

    a

--- resources/views/pescador/pages/index.blade.php
+++ resources/views/pescador/pages/index.blade.php
@@ -1,33 +1,38 @@
-@extends('layouts.app')
+@extends('layouts.app-pescador')
 
 @section('content')
-
-
-<div class="header">
+<div class="">
       <div class="container">
             <div class="py-4 text-center">
-                  <img class="img-fluid " src="{{ url('painel/img/logo.svg') }}" alt="">
+                  <img class="img-fluid " src="{{ url('app-comercial/img/logo.svg') }}" alt="">
             </div>
       </div>
 </div>
 <div class="main">
       <div class="title">
-            <div class="container px-4">
+            <div class="container px-4 d-flex justify-content-between">
                   <div class="pt-3">
-                        <h4>Olá,</h4>
-                        <h3>{{ auth()->user()->name  }}</h3>
+                        <h4>Olá, {{ auth()->user()->name  }} {{ auth()->user()->lastname  }}</h4>
+                  </div>
+                  <div>
+                        <a href="{{ route('consultor.logout') }}"> <span class="iconify" data-inline="false"
+                                    data-icon="ls:logout"
+                                    style="color: #ffffff; font-size: 38.42677688598633px;"></span></a>
                   </div>
             </div>
       </div>
-      <div class="title">
-            <div class="container">
-                  <div class="mt-3 d-flex px-3">
-                        <div>
-                              <h4>WALLET</h4>
-                        </div>
-                        <div class="mx-auto">
-                              <h3>€ 00,00</h3>
-                        </div>
+      <div class="container mt-2">
+            <div class="wallet d-flex px-4">
+                  <div class="py-4 ">
+
+                        <a href=""><i class="fas fa-wallet a-2"></i> Seu Wallet</a>
+                        <h3 class="balance">€ 00,00</h3>
+                  </div>
+                  <div class="col-4 eye">
+                        <span onclick="hide()"><i class="fas fa-eye"></i></span>
+                  </div>
+                  <div class="col-4 eye-close d-none">
+                        <span onclick="show()"><i class="fas fa-eye-slash"></i></span>
                   </div>
             </div>
       </div>
@@ -36,7 +41,7 @@
                   <div class="row pt-3 text-center">
                         <div class="col-4 text-center mt-3">
                               <a href="{{ route('pescador.produto') }}"><i class="fas fa-cart-plus a-1"></i>Vender
-                                    </a>
+                              </a>
                         </div>
                         <div class="col-4 mt-3">
                               <a href="{{ route('pescador.produto.list') }}"><i class="fas fa-fish a-2"></i>Produtos</a>
