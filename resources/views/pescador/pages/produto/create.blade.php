a87df2fb43108a85ae6ad004caa707e001038454
felipe <felipephplow@gmail.com>
2021-05-25 23:05:00 -0300

    a

--- resources/views/pescador/pages/produto/create.blade.php
+++ resources/views/pescador/pages/produto/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app-pescador')
 
 @section('content')
 
@@ -6,46 +6,65 @@
 <div class="header">
       <div class="container">
             <div class="py-4 text-center">
-                  <img class="img-fluid " src="{{ url('painel/img/logo.svg') }}" alt="">
+                  <img class="img-fluid " src="{{ url('app-comercial/img/logo-img.svg') }}" alt="">
             </div>
       </div>
 </div>
+<div>
+      <div class="d-flex justify-content-between container voltar py-4 mb-5">
+            <div>
+                  <a href="javascript:history.back()"> <i class="fas fa-chevron-left"></i> Voltar</a>
+            </div>
+            <div>
+                  <span>CADASTRO DE PEIXE</span>
+            </div>
+      </div>
+</div>
+@if ($errors->any())
+<div class="alert alert-danger">
+      <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+      </ul>
+</div>
+@endif
 
-<div class="container">
+<div class="container login-py">
       <form action="{{ route('pescador.produto.store') }}" method="POST">
             @csrf
             <div class="mt-3">
-                  <div class="form-group col-md-12">
-                        <label for="exampleFormControlSelect1">Espécie</label>
-                        <select class="form-control" name="especie_id" id="exampleFormControlSelect1">
+                  <div class="form-group input-material">
+                        <select class="form-control" name="especie_id" id="name_field">
                               @foreach ($especies as $especie)
+                              <option>Escolha a Espécie</option>
                               <option value="{{ $especie->id }}">{{ $especie->nome_portugues }}</option>
                               @endforeach
 
                         </select>
                   </div>
 
-                  <div class="form-group col-md-12">
-                        <label for="exampleFormControlSelect1">Espécie</label>
+                  <div class="form-group input-material">
+
                         <select class="form-control" name="porto_id" id="exampleFormControlSelect1">
                               @foreach ($portos as $porto)
+                              <option>Escolha o Porto</option>
                               <option value="{{ $porto->id }}">{{ $porto->nome }}</option>
                               @endforeach
 
                         </select>
                   </div>
 
-                  <div class="form-group col-md-12">
-                        <label for="exampleInputEmail1">Nome da Embarcação</label>
-                        <input type="text" required class="form-control" value="{{ auth()->user()->nome_embarcacao }}"
+                  <div class="form-group input-material">
+                        <input type="text" class="form-control" value="{{ auth()->user()->nome_embarcacao }}"
                               name="embarcacao">
+                        <label for="name-field">Nome da Embarcação</label>
                   </div>
-                  <div class="form-group col-md-12">
-                        <label for="exampleInputEmail1">Zona de Pesca</label>
-                        <input type="text" required class="form-control" name="zona">
+                  <div class="form-group input-material">
+                        <input type="text" class="form-control" name="zona">
+                        <label for="name-field">Zona de Pesca</label>
                   </div>
-                  <div class="form-group col-md-12">
-                        <label for="exampleFormControlSelect1">Tamanho</label>
+                  <div class="form-group input-material">
                         <select class="form-control" name="tamanho" id="exampleFormControlSelect1">
                               <option>Escolha o Tamanho</option>
                               <option value="tamanho1">Tamanho 1 (T1)</option>
@@ -54,8 +73,7 @@
                               <option value="tamanho4">Tamanho 4 (T4)</option>
                         </select>
                   </div>
-                  <div class="form-group col-md-12">
-                        <label for="exampleFormControlSelect1">Arte</label>
+                  <div class="form-group input-material">
                         <select class="form-control" name="arte" id="exampleFormControlSelect1">
                               <option>Escolha a Arte</option>
                               <option value="rede">Rede</option>
@@ -63,30 +81,25 @@
 
                         </select>
                   </div>
-                  <div class="form-group col-md-12">
-                        <label for="exampleFormControlSelect1">Quantidade</label>
-                        <input type="text" required class="form-control" name="quantidade">
+                  <div class="form-group input-material">
+                        <input type="number" class="form-control" name="quantidade">
+                        <label for="name-field">Quantidade</label>
                   </div>
-                  <div class="form-group col-md-12">
-                        <label for="exampleFormControlSelect1">Unidade</label>
+                  <div class="form-group input-material">
                         <select class="form-control" name="unidade" id="exampleFormControlSelect1">
                               <option value="unid">Unid</option>
                               <option value="kg">Kg</option>
                         </select>
                   </div>
-                  <div class="form-group col-md-12">
-                        <label for="exampleInputEmail1">Preço</label>
-                        <input type="text" required class="form-control" name="preco">
+                  <div class="form-group input-material">
+                        <input type="number" class="form-control" name="preco">
+                        <label for="name-field">Preço</label>
                   </div>
             </div>
-            <div class="form-group col-md-12">
-                  <label for="exampleInputEmail1"></label>
-                  <input type="submit" required class="form-control btn btn-dark" value="Cadastrar">
+            <div class="btn-box py-4"><button class="btn btn-submit" type="submit">Cadastrar E Vender</button>
+
             </div>
-            <div class="form-group col-md-12">
-                  <label for="exampleInputEmail1"></label>
-                  <a href=""> <input type="button" class="form-control btn btn-dark" value="Cancelar e Voltar"></a>
-            </div>
+
 </div>
 </form>
 
